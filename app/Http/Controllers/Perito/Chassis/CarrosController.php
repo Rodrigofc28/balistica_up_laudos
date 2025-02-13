3c4ba5c9bba481688961682cda166a196c410af4
Rodrigo <rodrigofc@live.com>
2025-02-13 11:17:35 -0300

    -

--- app/Http/Controllers/Perito/Chassis/CarrosController.php
+++ app/Http/Controllers/Perito/Chassis/CarrosController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 
-class CarroController extends Controller
+class CarrosController extends Controller
 {
    public function index(){
       
