c4a3a71ee8f3d6527dc10dcc86b5023d242878c5
Rodrigo <rodrigofc@live.com>
2025-03-06 08:57:50 -0300

    cadastro armas

--- public/js/cadastrar_opcao.js
+++ public/js/cadastrar_opcao.js
@@ -257,7 +257,7 @@ $('#trava_ferrolho').val(objetoBuscaCadastro.trava_ferrolho);
 $('#trava_ferrolho').trigger('change');
 $('#trava_gatilho').val(objetoBuscaCadastro.trava_gatilho);
 $('#trava_gatilho').trigger('change');
-$('#trava_seguranca').val(objetoBuscaCadastro.trava_seguranca);sistema_disparo
+$('#trava_seguranca').val(objetoBuscaCadastro.trava_seguranca);
 $('#trava_seguranca').trigger('change');
 
 $('#sistema_disparo').val(objetoBuscaCadastro.sistema_disparo);
--- resources/views/perito/laudo/attributes/inquerito.blade.php
+++ resources/views/perito/laudo/attributes/inquerito.blade.php
@@ -1,5 +1,5 @@
 <div class="col-lg-{{ $size ?? "3" }} mt-2">
-    <label for="inquerito"><strong>Nº Inquérito<strong><code>*</code></strong></strong></label>
+    <label for="inquerito"><strong>Nº do documento<strong><code>*</code></strong></strong></label>
     <input class="form-control{{ $errors->has('inquerito') ? ' is-invalid' : '' }}"
          id="inquerito"  name="inquerito" autocomplete="off" type="text"
       required     value="{{ old('inquerito', $inquerito) }}" maxlength="20"/>
